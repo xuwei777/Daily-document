f2c8ea7d688bd737432a124d43d3c609e1640f50
xuwei777 <42725791+xuwei777@users.noreply.github.com>
2019-04-24 11:45:32 +0800

    Delete zhenbei2.sh

--- zhenbei2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-find /tmp/zhenbei/ -daystart -type d -mtime +1 rm -rf {} \;
-zb2=$(find /tmp/zhenbei/ -daystart -name "`date '-d -1day' '+%F'`*")
-innobackupex --incremental --no-timestamp /tmp/zhenbei/`date '+%F'`_zb --incremental-basedir=$zb2 --user=admin --password=Admin.1root
-
-
-#* * */1 * *
